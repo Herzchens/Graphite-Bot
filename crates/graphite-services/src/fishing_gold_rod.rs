9cd62c6874b943569461aac5bb04d4586a2c615f
Itztli Herzchen <166841132+Herzchens@users.noreply.github.com>
2026-08-30 08:34:19 +0700

    style(services): preformat Gold Rod helper

--- crates/graphite-services/src/fishing_gold_rod.rs
+++ crates/graphite-services/src/fishing_gold_rod.rs
@@ -84,7 +84,9 @@ pub fn gold_fishing_rod_side_grade_policy(
     })
 }
 
-const fn relative_weight_multiplier(relative_increase_percent: u8) -> FishingRelativeWeightMultiplier {
+const fn relative_weight_multiplier(
+    relative_increase_percent: u8,
+) -> FishingRelativeWeightMultiplier {
     let numerator = 100_u16 + relative_increase_percent as u16;
     let denominator = 100_u16;
     let divisor = gcd(numerator, denominator);
